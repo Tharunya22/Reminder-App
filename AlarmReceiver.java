68b6009f2653340380f26f2de2d7fea6dc9855ab
tharunya a <69189893+Tharunya22@users.noreply.github.com>
2020-12-02 15:05:05 +0530

    Add files via upload

---
+++ AlarmReceiver.java
@@ -0,0 +1,54 @@
+package com.example.reminderapp;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.lifecycle.ViewModelProvider;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        //Get id & message from intent.
+        int notificationId = intent.getIntExtra("notificationId", 0);
+        String message = intent.getStringExtra("todo");
+
+        //When notification is tapped, call MainActivity.
+        Intent mainIntent = new Intent(context, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, mainIntent, 0);
+
+        NotificationManager mynotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        //Prepare notification.
+        Notification.Builder builder = new Notification.Builder(context);
+        builder.setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle("It's Time")
+                .setContentText(message)
+                .setWhen(System.currentTimeMillis())
+                .setAutoCancel(true)
+                .setContentIntent(contentIntent)
+                .setPriority(Notification.PRIORITY_MAX)
+                .setDefaults(Notification.DEFAULT_ALL);
+
+        //Notify
+        mynotificationManager.notify(notificationId, builder.build());
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
